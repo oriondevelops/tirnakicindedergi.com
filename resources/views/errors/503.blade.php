2d799f9281ba42dcf3f2bcd56a393bed218020de
orion <oriononair@gmail.com>
2022-07-02 20:46:23 +0300

    Add 503 page

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<title>çok yakında "sizlerle"</title>
+<script src="https://cdn.tailwindcss.com"></script>
+<style>
+    body {
+        text-align: center;
+        padding: 150px;
+    }
+
+    h1 {
+        font-size: 50px;
+    }
+
+    body {
+        font: 20px Helvetica, sans-serif;
+        color: #333;
+    }
+
+    article {
+        display: block;
+        text-align: left;
+        width: 650px;
+        margin: 0 auto;
+    }
+
+    a {
+        color: #dc8100;
+        text-decoration: none;
+    }
+
+    a:hover {
+        color: #333;
+        text-decoration: none;
+    }
+</style>
+
+<article class="prose space-y-6">
+    <img src="{{ asset('/media/logo.png') }}" class="w-24 h-24" alt="tırnak içinde dergi">
+    <h1 class="text-bold">çok yakında "sizlerle"</h1>
+    <p>"İnsan kendisiyle yaşayabilmesinin 'hamal gibi taşımak' anlamına gelmediğini çok yük taşıdıktan sonra anlıyor.
+        Ben bugüne kadar burnunu taşımaktan sıkıntı duyan, kulaklarının ağırlık yaptığını söyleyen bir insan görmedim.
+        Peki kendi parçalarından kaçmak niye? Anılarından, yazılarından, söylenmesi mümkün olmuş pişmanlıklarından...
+        Ateşse korkmak niye?"</p>
+    <div>
+        <p>&mdash; <b>Sevda Kenti Sakinleri, Osman Deveci</b></p>
+    </div>
+</article>
